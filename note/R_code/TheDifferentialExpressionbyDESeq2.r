5fed37bd0ae536bdd25b9e2c8c576900933db4e8
Shao.M.H <97825442+shaomh225@users.noreply.github.com>
2023-10-24 12:35:15 +0800

    Update TheDifferentialExpressionbyDESeq2.r

--- note/R_code/TheDifferentialExpressionbyDESeq2.r
+++ note/R_code/TheDifferentialExpressionbyDESeq2.r
@@ -1,25 +1,19 @@
-# 
-# Differential expression analysis with the DESeq2 package.
-# 
-# https://bioconductor.org/packages/release/bioc/html/DESeq2.html
-#
+
 # Install the DESeq2 package
-BiocManager::install("DESeq2", force = TRUE)
+BiocManager::install("DESeq2",force = TRUE)
 # Load the library.
-library(DESeq2)
-
-# The name of the file that contains the counts.
-counts_file = "counts.csv"
-
-# The sample file is in CSV format and must have the headers "sample" and "condition".
-design_file = "design.csv"
-
-# The final result file.
-output_file = "results.csv"
-
-# Read the sample file.
-colData <- read.csv(design_file, stringsAsFactors=F)
-
+library("DESeq2")
+# The pathway to the file that contains the counts
+counts_file <- "H:\\Project\\downsteam\\different_expression\\merged_kallisto_counts.csv"
+# The pathway to the file that contains design infomations
+design_file <- "H:\\Project\\downsteam\\different_expression\\design.csv"
+# The final result file
+output_file <- "H:\\Project\\downsteam\\different_expression\\result_VsaFsa.csv"
+# Read the sample file
+original_colData <- read.csv(design_file, stringsAsFactors = F)
+# Extract some groups that want to focus on
+filter_condition <- original_colData$condition %in% c("VSA", "FSA")
+colData <- original_colData[filter_condition,]
 # Turn conditions into factors.
 colData$condition = factor(colData$condition)
 
@@ -31,14 +25,16 @@ colData$condition = relevel(colData$condition, toString(colData$condition[1]))
 sample_names <- colData$sample
 
 # Read the data from the standard input.
-df = read.csv(counts_file, header=TRUE, row.names=1 )
+original_df = read.csv(counts_file, header=TRUE, row.names=1 )
+# Extract some columns that want to focus on
+df = subset(original_df, select = c("KS_PF_1","KS_PF_2","KS_PF_3",
+                                    "SH_PF_1","SH_PF_2","SH_PF_3"))
 
 # Created rounded integers for the count data
 countData = round(df[, sample_names])
 
 # Other columns in the dataframe that are not sample information. 
 otherCols = df[!(names(df) %in% sample_names)]
-
 #
 # Running DESeq2
 #
@@ -79,6 +75,7 @@ data$falsePos = 1:nrow(data) * data$FDR
 data$baseMeanA = 1
 data$baseMeanB = 1
 
+
 # Get the normalized counts.
 normed = counts(dse, normalized=TRUE)
 
@@ -91,15 +88,17 @@ total <- merge(data, normed, by=0)
 # Sort again for output.
 total = total[with(total, order(PValue, -foldChange)), ]
 
-# Sample names for condition A
+# Sample names for condition A:KS_PF
 col_names_A = data.frame(split(colData, colData$condition)[1])[,1]
 
-# Sample names for condition B
+# Sample names for condition B:SH_PF
 col_names_B = data.frame(split(colData, colData$condition)[2])[,1]
+
 
 # Create the individual baseMean columns.
 total$baseMeanA = rowMeans(total[, col_names_A])
 total$baseMeanB = rowMeans(total[, col_names_B])
+
 
 # Bringing some sanity to numbers. Round columns to fewer digits.
 total$foldChange = round(total$foldChange, 3)
@@ -125,15 +124,18 @@ new_cols = c("name", names(otherCols), "baseMean","baseMeanA","baseMeanB","foldC
 
 # Slice the dataframe with new columns.
 total = total[, new_cols]
+names(total)[names(total)=="baseMeanA"] <- "VSA"
+names(total)[names(total)=="baseMeanB"] <- "FSA"
+total_VsaFsa <- total
+
+# Remove the specific row whose "baseMean" is 0
+total_VsaFsa_Remove0 = subset(total_VsaFsa, total_VsaFsa$baseMean != 0)
+
 
 # Write the results to the standard output.
-write.csv(total, file=output_file, row.names=FALSE, quote=FALSE)
-
-# Inform the user.
-print("# Tool: DESeq2")
-print(paste("# Design: ", design_file))
-print(paste("# Input: ", counts_file))
-print(paste("# Output: ", output_file))
+write.csv(total_VsaFsa_Remove0,
+          file="H:\\Project\\downsteam\\different_expression\\result_VsaFsa_remove0.csv",
+          row.names=FALSE, quote=FALSE)
 
 
 
