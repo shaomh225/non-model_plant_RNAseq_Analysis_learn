c9656b8d1a47262a8d365a78ccec90f65dd0f9cf
Shao.M.H <97825442+shaomh225@users.noreply.github.com>
2023-10-24 12:41:25 +0800

    Update TheDifferentialExpressionbyDESeq2.r

--- note/R_code/TheDifferentialExpressionbyDESeq2.r
+++ note/R_code/TheDifferentialExpressionbyDESeq2.r
@@ -143,6 +143,6 @@ write.csv(total_VsaFsa_Remove0,
 
 
 
-###Acknowlegement:
+###Citation&Acknowlegement:
 ###This R code is based on Website "https://www.biostarhandbook.com/books/rnaseq/differential-expression.html"
 ###and I make some adjustments to suit for my data.
